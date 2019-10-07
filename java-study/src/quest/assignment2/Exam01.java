ca148e7d3a6d57b25d7dfba74adb84b4ba78e2f1
Sun ju Kim <ksunju0610@naver.com>
2019-10-07 20:52:06 +0900

    Assignment 2 - Example 1 완성.

--- java-study/src/quest/assignment2/Exam01.java
+++ java-study/src/quest/assignment2/Exam01.java
@@ -16,7 +16,19 @@
 할인된 금액은 190000 입니다. 
  */
 package quest.assignment2;
+import java.util.Scanner;
 
 public class Exam01 {
-
+	
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.print("물건값을 입력하세요(만원) : ");
+	int money = Integer.parseInt(sc.nextLine());
+	
+	
+	double discount = money <= 10 ? money* 0.97 : money<= 30 ? money*0.95 : money<=100? money*0.92 : money*0.9;
+	discount*=10000;
+	System.out.print("할인적용 금액은 " + (int)discount + " 원 입니다.");
+	}
 }
