ba014e18da444b4fd3b636bab293785f561245a7
Sun ju Kim <ksunju0610@naver.com>
2019-10-09 14:43:42 +0900

    assign3 - exam2,3 수정

--- java-study/src/quest/assignment3/loop/Exam02.java
+++ java-study/src/quest/assignment3/loop/Exam02.java
@@ -15,20 +15,17 @@ public class Exam02 {
 	public static void main(String[] args) {
 		
 		Random r = new Random();
-		int[] randomNum = new int[5];
+		int randomNum =0;
 		int sum = 0;
 		double average = 0;
 		
 		System.out.print("생성된 수 : ");
 		for(int i=0;i<5;i++)	{
-			randomNum[i] = r.nextInt(100)+1;
-			System.out.print(randomNum[i]+ " ");
-			sum += randomNum[i];
-		}average = sum / 5.00F;
+			randomNum = r.nextInt(100)+1;
+			System.out.print(randomNum+ " ");
+			sum += randomNum;
+		}average = sum / 5.d;
 		System.out.println("\n합계 : " + sum);
 		System.out.println("평균 : " + String.format("%.2f", average));
-			
 		
-	}
-	
-}
+	}}
--- java-study/src/quest/assignment3/loop/Exam03.java
+++ java-study/src/quest/assignment3/loop/Exam03.java
@@ -8,7 +8,25 @@
 
  */
 package quest.assignment3.loop;
+import java.util.Scanner;
 
 public class Exam03 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("수를 입력하세요(2 - 10) : ");
+		int input = Integer.parseInt(sc.nextLine());
+		while(input>10 || input<2) {
+			System.out.print("다시 입력하세요 : ");
+			input = Integer.parseInt(sc.nextLine());
+		}System.out.print(input + "의 배수 : ");
+		for (int i=1;i<=10;i++) {
+			System.out.print(input*i+" ");
+		}
+		
+		
+		
+	}
 
 }
