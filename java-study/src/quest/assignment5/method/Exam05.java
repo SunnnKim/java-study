5f45d73b0747f8be296b8778f51784a8e18caff7
Sun ju Kim <ksunju0610@naver.com>
2019-10-17 17:06:54 +0900

    Assignment 5 - Exam 5 수정 및 재풀이

--- java-study/src/quest/assignment5/method/Exam05.java
+++ java-study/src/quest/assignment5/method/Exam05.java
@@ -1,4 +1,5 @@
 package quest.assignment5.method;
+import java.util.*;
 
 /*
 매개변수로 받은 문자열에서 마지막 숫자를 하나 증가시켜서 리턴하는 메서드를 구현하시오. 
@@ -10,44 +11,44 @@ PRODUCT-00004
 */
 class Exam05 { 
 	public static void main(String[] args) { 
-		System.out.println(getNextId("USER-00007")); 
-		System.out.println(getNextId("ORDER-0009")); 
-		System.out.println(getNextId("PRODUCT-00003")); 
-
+		System.out.println(getNextId("USER-00027")); 
+		System.out.println(getNextId("ORDER-0099")); 
+		System.out.println(getNextId("PRODUCT-00003"));
 		
 	} 
 	private static String getNextId(String id) { 
 		//기능구현 
-		int length = id.length();
-		char [] chs = new char[length];
-		chs = id.toCharArray();
-		int lastNum = chs[length-1] - '0';
+
+		String [] str = id.split("-");
 		
-		if( lastNum == 9 ) {
-			chs[length-1] = '0';
-			lastNum = chs[length-2] - '0';
-			lastNum++;
-			chs[length-2] = Character.forDigit(lastNum,10);
-		}else {
-			lastNum++;
-			chs[length-1] = Character.forDigit(lastNum,10);
+		int length = str[1].length();
+		int count=0;
+		
+		for(int i=0; i<length; i++) {
+			if(str[1].charAt(i) == '0' ) count++;
+			else break;
+		} // 몇자리부터 0이 아닌 숫자인지 계산하기
+		
+		int mul = 1;
+		int sum = 0;
+		for(int i=1; i<=length - count ; i++) {
+			sum += (str[1].charAt(length-i) - '0') * mul;
+			mul *= 10;
 		}
-		String result="";
-		for(int i=0;i<length;i++) 
-				result+= chs[i];
-			
+		sum++;
+		mul = 10;
+
+		char[] chs = new char[ length ]; //새로운 str[1] 값의 저장소
 		
-		return result;
+		for(int i=length-1; i>=0; i--) {
+			if(sum>0) { 
+			chs[i] = Character.forDigit(sum % mul,10); 
+			sum = sum / 10; 
+			}else chs[i] = '0'; //더이상 새로운 수가 없을 경우 0 저장
+		}
+		String newStr = str[0] +"-"+ String.valueOf(chs); //결과!!
+		
+
+		return newStr;
 	} 
 }
-/*
- * String getId = Character.toString(id.charAt(id.length()-1));
-		String newStr="";
-		
-		int addId = Integer.parseInt(getId) +1;
-		String newId = Integer.toString(addId);
-		newStr = id.replace(getId, newId);
-		int a = 123;
-		String.valueOf(a);
-		*/
- 
