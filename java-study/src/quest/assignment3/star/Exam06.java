d119662e9a32e1385944fbb1d4b58a496a30fa53
Sun ju Kim <ksunju0610@naver.com>
2019-11-04 22:18:05 +0900

    no message

--- java-study/src/quest/assignment3/star/Exam06.java
+++ java-study/src/quest/assignment3/star/Exam06.java
@@ -20,7 +20,7 @@ public class Exam06 {
 		for(int i=1;i<=5;i++) { //행
 			
 			for(int j=5;j>i;j--) System.out.print(" ");
-			for(int k=0;k<2*i-1;k++) System.out.print("*");
+			for(int j=0;j<2*i-1;j++) System.out.print("*");
 			System.out.println("");
 			
 		 }
