84fabdb69e02fcac601943ab24073d1a0c606fdf
Sun ju Kim <ksunju0610@naver.com>
2020-06-02 00:55:39 +0900

    algorithm : Network 내풀이

--- Algorithm/src/algorithm/programmers/bfsdfs/Network2.java
+++ Algorithm/src/algorithm/programmers/bfsdfs/Network2.java
@@ -6,8 +6,8 @@ public class Network2 {
 		
 		int[][] computers = {
 			{1,1,0},
-			{1,1,1},
-			{0,1,1}
+			{1,1,0},
+			{0,0,1}
 		};
 		System.out.println(s.solution(3, computers));
 	}
@@ -24,7 +24,8 @@ class Network1 {
 		// dfs : 행별로 검사 시작
 		for(int i = 0 ; i < n; i++ ) {
 			if(!check[i]) {
-				dfs(i, 0, computers );
+				connection++;
+				dfs(i, computers );
 			}
 		}
 		
@@ -32,16 +33,15 @@ class Network1 {
 		return connection;
 	}
 	
-	public void dfs(int row, int column, int[][] arr ) {
+	public void dfs(int n, int[][] arr ) {
 		// 행 검사 시작
-		check[row] = true;
-		for( int i = column; i < arr.length; i++ ) {
-			if( arr[row][column] == 1 && check[column] == false) {
-			
-			}else {
-			
+		check[n] = true;
+		for(int i = 0; i < arr.length; i++ ) {
+			if( arr[n][i] == 1 && !check[i] ) {
+				dfs(i, arr);
 			}
 		}
+		
 	}
 	
 	
