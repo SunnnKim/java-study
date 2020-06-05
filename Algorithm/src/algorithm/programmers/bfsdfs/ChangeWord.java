0a6896bdfddf8808eae75b4053ef064bd0e59a76
Sun ju Kim <ksunju0610@naver.com>
2020-06-05 17:20:27 +0900

    알고리즘 연습

---
+++ Algorithm/src/algorithm/programmers/bfsdfs/ChangeWord.java
@@ -0,0 +1,111 @@
+package algorithm.programmers.bfsdfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ 두 개의 단어 begin, target과 단어의 집합 words가 있습니다. 
+ 아래와 같은 규칙을 이용하여 begin에서 target으로 변환하는 가장 짧은 변환 과정을 찾으려고 합니다.
+
+1. 한 번에 한 개의 알파벳만 바꿀 수 있습니다.
+2. words에 있는 단어로만 변환할 수 있습니다.
+예를 들어 begin이 hit, target가 cog, words가 
+[hot,dot,dog,lot,log,cog]라면 hit -> hot -> dot -> dog -> cog와 같이 
+4단계를 거쳐 변환할 수 있습니다.
+
+두 개의 단어 begin, target과 단어의 집합 words가 매개변수로 주어질 때, 
+최소 몇 단계의 과정을 거쳐 begin을 target으로 변환할 수 있는지 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+각 단어는 알파벳 소문자로만 이루어져 있습니다.
+각 단어의 길이는 3 이상 10 이하이며 모든 단어의 길이는 같습니다.
+words에는 3개 이상 50개 이하의 단어가 있으며 중복되는 단어는 없습니다.
+begin과 target은 같지 않습니다.
+변환할 수 없는 경우에는 0를 return 합니다.
+입출력 예
+begin	target	words	return
+hit	cog	[hot, dot, dog, lot, log, cog]	4
+hit	cog	[hot, dot, dog, lot, log]	0
+입출력 예 설명
+예제 #1
+문제에 나온 예와 같습니다.
+
+예제 #2
+target인 cog는 words 안에 없기 때문에 변환할 수 없습니다.
+ */
+public class ChangeWord {
+	 public static void main(String[] args) {
+		 
+		 String[] words = { "hot", "dot", "dog", "lot", "log", "cog" };
+		 String begin = "hit";
+		 String target = "cog";
+		 SolutionBFS s = new SolutionBFS();
+		 System.out.println(s.solution(begin, target, words));
+		 
+	 }
+	 
+	 
+    
+}
+// 큐로풀기??
+
+
+// 답 
+
+class SolutionBFS {
+
+    static class Node {
+        String next;
+        int edge;
+
+        public Node(String next, int edge) {
+            this.next = next;
+            this.edge = edge;
+        }
+    }
+
+    public int solution(String begin, String target, String[] words) {
+        int n = words.length, ans = 0;
+
+        // for (int i=0; i<n; i++)
+        //  if (words[i] != target && i == n-1) return 0;
+
+        Queue<Node> q = new LinkedList<>();
+
+
+        boolean[] visit = new boolean[n];
+        q.add(new Node(begin, 0));
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            if (cur.next.equals(target)) {
+                ans = cur.edge;
+                break;
+            }
+
+            for (int i=0; i<n; i++) {
+                if (!visit[i] && isNext(cur.next, words[i])) {
+                    visit[i] = true;
+                    q.add(new Node(words[i], cur.edge + 1));
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    static boolean isNext(String cur, String n) {
+        int cnt = 0;
+        for (int i=0; i<n.length(); i++) {
+            if (cur.charAt(i) != n.charAt(i)) {
+                if (++ cnt > 1) return false;
+            }
+        }
+
+        return true;
+    }    
+}
+
+
